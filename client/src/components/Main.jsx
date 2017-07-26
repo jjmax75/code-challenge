503e84b885e6b2f8ee8471b0cac89d0fc733a21e
John Behan <behanj@gmail.com>
2017-07-26 23:17:54 +0700

    match details

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -13,6 +13,30 @@ const handlePlayMatchReport = (report) => {
   console.log('play match report:', report);
 };
 
+const getMatchDate = (date, tz) => {
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+    'August', 'September', 'October', 'November', 'December'];
+  const getOrdinal = (num) => {
+    if (num > 3 && num < 21) return 'th';
+    switch (num % 10) {
+      case 1: return 'st';
+      case 2: return 'nd';
+      case 3: return 'rd';
+      default: return 'th';
+    }
+  };
+  const d = new Date(date);
+  const day = d.getDate();
+  const month = months[d.getMonth()];
+  const year = d.getFullYear();
+  const hours = d.getHours();
+  const minutes = d.getMinutes();
+  const dateString =
+    `${day}${getOrdinal(day)} ${month}, ${year}, ${hours}:${minutes} ${tz}`;
+
+  return dateString;
+};
+
 export const App = props => (
   <div>
     <header>
@@ -23,6 +47,12 @@ export const App = props => (
       />
     </header>
     <main>
+      <div className='match-details--title'>
+        { props.allData.name }
+      </div>
+      <div className='match-details--date'>
+        { getMatchDate(props.allData.local_date, props.allData.tz) }
+      </div>
       <TeamDetails teamData={props.teamData} />
       <MatchDetails allData={props.allData} />
       <Filters handleFilterChange={props.filterEvents} />
--- client/src/components/matchDetails/index.jsx
+++ client/src/components/matchDetails/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './matchDetails.scss';
 
 const MatchDetails = props => (
-  <div>
-    {props.allData.local_date}
+  <div className='match-details'>
+    <div className='match-details__competition'>
+      {props.allData.competition_name}
+    </div>
+    <div className='match-details__venue'>
+      {props.allData.venue_name}, {props.allData.city}
+    </div>
+    <div className='match-details__referee'>
+      Referee: {props.allData.referee}
+    </div>
   </div>
 );
 
